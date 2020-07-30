3d5f1547f30199d1a0852689dea8d9de9210c8d5
Carlos Aravena <carlos.aravena@gmail.com>
2020-07-30 12:18:31 -0400

    Se agregan las preguntas como comentario

---
+++ control_002/control_002_CarlosAravena.R
@@ -0,0 +1,153 @@
+### Control 2: Manipulación de tablas y gráficos ###
+#
+# Nombre Integrantes : Carlos Aravena De Los Ríos
+# Curso              : Introduccion a R 2020-2
+# Sigla              : DBDN-R-C2
+
+####### Librerias a utilizar
+library("dplyr")
+
+
+############################## Sección 1 ##############################
+### Preguntas 1.1
+# P1a) (1pt) Basándose en a tabla general, ¿cuántos restaurants distintos hay en total?
+# P1b) (1pt) ¿En cuántos tipos de comida diferentes se clasifican los restaurants?
+# P1c) (2pt) ¿Cuántas ciudades distintas considera el sondeo?
+# P1d) (2pt) ¿Indique el tipo de comida y las ciudades donde se encuentra el restaurant
+#             "great wall restaurant"?
+# P1e) (2pt) ¿Cuántos restaurantes de la ciudad de san francisco tienen calificación
+#             mayor o igual a 3.8 y venden comida vegetariana (vegetarian) ?
+
+
+### Preguntas 1.2
+# P2a) (2pt) Sin considerar San Francisco, ¿cuál es la ciudad con mayor cantidad
+#            de restaurantes sondeados?
+# P2b) (1pt) ¿Cuáles son los 3 tipos de comida ofrecido más comunes ?
+# P2c) (2pt) Sin con siderar San Francisco, ¿Cuáles son las 3 ciudades con mayor
+#            cantidad de restaurants que ofrecen comido tipo japanese?
+# P2d) (2pt) Usted decide viajar a una de las ciudades en cuestión, para ello
+#            calcula el promedio de las valoraciones medias (promedio de review)
+#            por cada ciudad, y escoje aquella con mayor review promedio.
+#            ¿Qué ciudad escoge?
+# P2e) (2pt) Cuál es la ciudad con mejor valoración promedio de restaurantes
+#            tipo "barbeque"
+
+
+
+
+
+### Preguntas 1.3
+# P3a) (4pt) En la pregunta 1d), se pudo observar que un mismo restaurant puede
+#            estar presente en más de una ciudad. ¿Cuántos restaurants tienen
+#            esta característica, es decir están en más de una ciudad distinta?
+#            De ser de utilidad puede investigar y utilizar la función distinct().
+# P3b) (2pt) ¿Cuál es el restaurant que tiene presencia en la mayor cantidad de
+#             ciudades distintas?¿En cuántas ciudades está presente?
+# P3c) (5pt) Diremos que un restaurant posee sucursales si en la tabla general
+#            existe más de un registro con el mismo label. Muestre, mediante un
+#            gráfico de barras, los 15 restaurants con mayor cantidad de
+#            sucursales, donde la altura de la barra representa el total
+#            de sucursales de cada restaurant. Su gráfico debe contemplar
+#            al menos los siguientes aspectos:
+#               - Debe contener un título.
+#               - Debe incluir nombres sugerentes en los ejes.
+#               - Los nombres de los restaurants deben ser legibles.
+#               - Los restaurants deben ir ordenados según su cantidad de sucursales.
+#               - Incluya tiquetas en la parte superior de cada barra,
+#                 que muestre la cantidad de sucursales respectivas.
+#               - Las leyendas de cada gráfico no deben visualizarse
+
+
+
+
+### Preguntas 1.4
+# P4a) (4pts) Genere una tabla llamada resumen, que contenga la siguiente
+#             información:
+#                - city: Ciudad
+#                - food_type: Tipo de comida
+#                - n_rest: Cantidad de restaurantes por cada ciudad y tipo de comida.
+#                - review_prom: Valoración promedio por cada ciudad y tipo de comida.
+#                - total_rest: Total de restaurantes por cada ciudad (se puede
+#                  repetir el valor por cada tipo de comida).
+#                - review_prom_city: Valoración promedio de los restaurantes por
+#                  cada ciudad (se puede repetir el valor por cada tipo de comida).
+#
+# Su tabla deberá tener la siguiente estructura:
+#
+#          city   food_type    n_rest   review_prom   total_rest   review_prom_city
+# -------------   ---------    ------   -----------   ----------   ----------------
+# san francisco     chinese       172      2.345349         1241           2.490250
+# san francisco     italian       142      2.641549         1241           2.490250
+#      san jose       pizza       126      2.131746          933           2.193569
+#      san jose        cafe       121      2.033884          933           2.193569
+#       oakland     chinese       112      2.156250          666           2.194745
+#       oakland        cafe        90      2.051111          666           2.194745
+#
+#
+#
+# P4b) (2pts) Basado en la tabla anterior, construya dos nuevas columnas llamadas
+#             density_food_type y ratio_review que contengan la siguiente información:
+#                - density_food_type: Representa el cuociente entre le total de
+#                  restaurants por tipo de comida y ciudad, respecto del total
+#                  de restaurantes de la ciudad. (n_rest/total_rest)
+#                - ratio_review: Representa el cociente entre a valoración
+#                  del restaurant por tipo de comida y ciudad, respecto de la
+#                  valoración promedio de los resturants de la misma ciudad.
+#                  (review_prom/review_prom_city)
+#
+# P4c) (3pts) Mediante un gráfico de dispersión, muestre la relación entre
+#             density_food_type y ratio_review. Investigue sobre el parámetro
+#             alpha dentro de la capa geométrica para una mejor visualizaciónd
+#             de los puntos. Adicionalmente añada una curva de tendencia y, con
+#             base en él, indique si cabe la posibilidad de establecer algún
+#             tipo de dependencia entre density_food_type y ratio_review.
+
+
+# Preguntas 1.5
+# P5a) (3pts) En la tabla resumen creada en P4a), genere una nueva columna
+#             llamada type_review, que contenga "review alto",
+#             si ratio_review >= 1 y "review bajo" ratio_review < 1.
+#             ¿Qué indica esta variable? Comente.
+# P5b) (3pts) Para cada type_review, muestre a través de un gráfico de
+#             cajas (boxplot), la distribución de la densidad del tipo de
+#             comidadensity_food_type. ¿Qué puede observar?
+
+
+### Pregunta 1.6
+# P6a) (2pts) Determine cuáles son las iguientes ciudades:
+#
+#     Ciudad 1: Ciudad con mayor cantidad de restaurants (posición 1).
+#     Ciudad 2: Ciudad ubicada en la posición 5, al ordenar las ciudades
+#               de manera decreciente según cantidad de restaurants.
+#     Ciudad 3: Ciudad ubicada en la posición 10, al ordenar las ciudades
+#               de manera decreciente según cantidad de restaurants.
+
+
+# P6b) (6pts) Usted deberá graficar la cantidad de restaurants, por cada uno
+#             de los 5 tipos de comida más frecuentes dentro de la ciudad
+#             respectiva. Las ciudades graficadas deben corresponder, de
+#             izquierda a derecha, a las ciudades de posición 1, 5 y 10
+#             encontradas en P6a). Al generar el gráfico, deberá tener
+#             en cuenta los siguientes elementos:
+#                - Investigue sobre el parámetro position dentro de la capa
+#                  geométrica que genera el gráfico de barras, para la correcta
+#                  presentación de las mismas.
+#                - Por cada ciudad, sólo se grafican los 5 tipos de comida más
+#                  frecuentes (con mayor cantidad de restaurants de dicho tipo).
+#                - EL gráfico deberá mostrar las barras ordenadas por altura tal
+#                  cual se muestra en la imagen. De ser de utilidad, puede
+#                  investigar el parámetro group dentro de la función aes().
+#                - Debe incluir título, nombre de los ejes y nombre de la leyenda,
+#                  tal como se muestra en la imagen.
+#                - Incluya etiquetas en la parte superior de cada barra. Estas
+#                  etiquetas deberán mostrar la densidad del tipo de comida
+#                  density_food_type, expresado como porccentaje (ie. density_food_type*100 %).
+#                  Para tal efecto, investigue sobre la utilización de las
+#                  funciones geom_label() o geom_text().
+#
+# NOTA: Como referencia, su gráfico debe contener los mismos elementos del cuarto
+#       gráfico, "Cantidad de restaurantes por tipo de comida y ciudad", que se
+#       en el README.md (no necesariamente los mismos valores de los elementos que lo componen).
+#       Por otro lado, no es necesaria la utilización de la misma paleta de
+#       colores. En este gráfico se muestran las ciudades de posición 1, 2 y 3.
+#       Ustede deberá visualizar las 1, 5 y 10.
